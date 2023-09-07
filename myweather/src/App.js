1cae542e1d90793e5135d69b8ef4005f1fd85d5f
Halashankaraswamy PBM <95228398+HalashankaraswamyPBM@users.noreply.github.com>
2023-09-07 16:09:30 +0530

    weather
    ..

---
+++ myweather/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+import Weather from './Weather';
+
+function App() {
+  return (
+    <div className="App">
+      <Weather />
+    </div>
+  );
+}
+
+export default App;
